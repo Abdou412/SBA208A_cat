2d00a76cd1c5f9a4eae0ff81826bc487a498accf
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2025-01-12 19:20:33 -0500

    api file

---
+++ api.js
@@ -0,0 +1,19 @@
+const API_URL = "https://catfact.ninja/fact";
+
+export const fetc_hcat_facts = async () => {
+  const response = await fetch(API_URL);
+  if (!response.ok) throw new Error("Failed to fetch data");
+  return await response.json();
+};
+
+export const post_cat_fact = async (fact) => {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ fact }),
+  });
+  if (!response.ok) throw new Error("Failed to add fact");
+  return await response.json();
+};
